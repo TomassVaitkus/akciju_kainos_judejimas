b22862e7de9adaf9176c0ec8ccc3ee215086e870
Shliosen <tv.vaitkus@gmail.com>
2019-10-08 11:35:14 +0300

    gaunami duomenys main.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-stock_id = 'IBM'
-open_price = 1.258
-close_price = 1.2333
-price_low = 1.4555
-price_max = 1.547
-volume = 4202500.0
-one_entry = [stock_id, open_price, close_price, price_low, price_max, volume]
+import pandas as pd
+import pandas_datareader as web
+import datetime as dt
+from matplotlib import style
+
+style.use('ggplot')
+# stock_id = ['IBM', 'TSLA', 'GOOGL', 'FB', 'AAPL']
+# reikia talpinti daugiau akciju ID, kad ivedus GUI input laukelyje akcijos ID ismestu rezultata  tos akcijos grafika
+
+start = dt.datetime( 2019,1,1)
+end = dt.datetime(2019,6,1)
+
+df = web.DataReader('IBM', 'yahoo', start, end)
+print(df.head())
+
+df1 = web.DataReader('TSLA', 'yahoo', start, end)
+print(df1.head())
+#
+# one_entry = [stock_id, open_price, close_price, price_low, price_max, volume]
