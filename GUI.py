8fbb1178880f48306cf2136a4e179c314f9fc241
Shliosen <tv.vaitkus@gmail.com>
2019-10-10 16:20:15 +0300

    Pridetas atvaizdavimas

--- GUI.py
+++ GUI.py
@@ -17,16 +17,16 @@ root = tk.Tk()
 canvas = tk.Canvas(root, height=HEIGHT, width = WIDTH)
 canvas.pack()
 
-frame = tk.Frame(root, bg = '#809fff')
+frame = tk.Frame(root, bg = '#ccccff')
 frame.place(relx = 0.1, rely = 0.1, relwidth = 0.8, relheight = 0.8)
 
 
-button = tk.Button(frame, text = "bandymo mygtukas", bg = 'grey', fg = 'red')
+button = tk.Button(frame, text = "bandymo mygtukas", bg = 'grey', fg = '#e6e6ff')
 button.pack()
 
-label = tk.Label(frame, text = "Teksto vieta", bg = 'yellow')
+label = tk.Label(frame, text = "Teksto vieta", bg = '#a3a375')
 label.pack()
 
-entry = tk.Entry(frame, text = "Teksto ivestis", bg = '#ff3333')
+entry = tk.Entry(frame, text = "Teksto ivestis", bg = '#f5f5f0')
 entry.pack()
 root.mainloop()
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+# informacijos saltinis https://pythonprogramming.net
+
+
 import pandas as pd
 import pandas_datareader as web
 import datetime as dt
 from matplotlib import style
+import matplotlib.pyplot as plt
 
 style.use('ggplot')
 # stock_id = ['IBM', 'TSLA', 'GOOGL', 'FB', 'AAPL']
@@ -10,10 +14,24 @@ style.use('ggplot')
 start = dt.datetime( 2019,1,1)
 end = dt.datetime(2019,6,1)
 
-df = web.DataReader('IBM', 'yahoo', start, end)
+df = web.DataReader('TSLA', 'yahoo', start, end)
+df.to_csv('TSLA.csv')
+
+df = pd.read_csv('TSLA.csv', parse_dates = True, index_col = 0)
+df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
 print(df.head())
 
-df1 = web.DataReader('TSLA', 'yahoo', start, end)
-print(df1.head())
-#
+ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
+
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100ma'])
+ax2.bar(df.index, df['Volume'])
+
+
+plt.show()
+
+# df1 = web.DataReader('TSLA', 'yahoo', start, end)
+
+
 # one_entry = [stock_id, open_price, close_price, price_low, price_max, volume]
